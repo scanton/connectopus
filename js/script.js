cb5b91fd893118abc581f02c21b91445636c97cd
Satori Canton <satoricanton@gmail.com>
2017-05-08 15:45:13 -0500

    added support for partials

--- js/script.js
+++ js/script.js
@@ -1,2 +1,36 @@
 const tunnel = require('tunnel-ssh');
 const mysql = require('mysql');
+
+const config = require('./working_files/config.js');
+
+/** config.js (example)
+module.exports = {
+	servers: [
+		{
+			name: 'Server Name',
+			server: '<server url>.com',
+			port: 80,
+			username: '<username>',
+			password: '<password>',
+			connections: [
+				{
+					name: 'MySQL on <Server Name>',
+					type: 'MySQL',
+					host: 'localhost',
+					database: '<db name>',
+					username: '<db user>',
+					password: '<db password>'
+				}
+			]
+		}
+	]
+}
+ **/
+
+ $(document).ready(function() {
+ 	$(".include-partial").each(function() {
+ 		var $this = $(this);
+ 		var uri = $this.attr("data-target");
+ 		$this.load(uri);
+ 	});
+ });
