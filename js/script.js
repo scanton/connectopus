b8604c033879187deb475685f149f355a19f2bd0
scanton <satoricanton@gmail.com>
2017-05-09 13:54:43 -0500

    Adding server list

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const tunnel = require('tunnel-ssh');
 const mysql = require('mysql');
 
-const config = require('./working_files/config.js');
+var config = {};
+
+//const config = require('./working_files/config.js');
 
 /** config.js (example)
 module.exports = {
@@ -26,11 +28,62 @@ module.exports = {
 	]
 }
  **/
+var renderMysqlDetails = function(data) {
+	var s = '';
 
- $(document).ready(function() {
+	return s;
+}
+
+var renderServerDetails = function(data) {
+	var s = '';
+	if(data) {
+		s += '<div class="input-group"><span class="input-group-addon">Server</span><input type="text" value="' + data.server + '" /></div>';
+		s += '<div class="input-group"><span class="input-group-addon">Port</span><input type="text" value="' + data.port + '" /></div>';
+		s += '<div class="input-group"><span class="input-group-addon">Username</span><input type="text" value="' + data.username + '" /></div>';
+		s += '<div class="input-group"><span class="input-group-addon">Password</span><input type="text" value="' + data.password + '" /></div>';
+		if(data.connections) {
+			for(var i in data.connections) {
+				s+= renderMysqlDetails(data.connections[i]);
+			}
+		}
+	}
+	return s;
+}
+
+var renderServers = function(data) {
+ 	var s = '';
+ 	if(data.servers) {
+ 		var servers = data.servers;
+ 		s += '<ul class="list-group server-list-group">';
+ 		for(var i in servers) {
+ 			var server = servers[i];
+ 			if(server) {
+ 				s += '<li class="list-group-item">' + server.name + '<span class="badge">' + server.connections.length + '</span>';
+ 				s += '<div class="server-details" style="display: none;">';
+ 				s += renderServerDetails(server);
+ 				s += '</div></li>';
+ 			}
+ 		}
+ 		s += '</ul>';
+ 	}
+ 	return s;
+}
+$(document).on("click", ".server-list-group .list-group-item", function (evt) {
+	var $this = $(this);
+	$this.find(".server-details").toggle('slow');
+});
+
+$(document).ready(function() {
+ 	$.ajax('./working_files/config.json').done(function(data) {
+ 		if(data) {
+ 			config = JSON.parse(data);
+ 			$(".server-list").html(renderServers(config));
+ 		}
+ 	});
  	$(".include-partial").each(function() {
  		var $this = $(this);
  		var uri = $this.attr("data-target");
  		$this.load(uri);
  	});
  });
+
