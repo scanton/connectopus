21c9a695aaf90abdd85c2f0912b3c17b65f4c23a
Satori Canton <satoricanton@gmail.com>
2017-05-10 18:40:04 -0500

    encapsulated html rendering into a class

---
+++ custom_modules/HtmlRenderer.js
@@ -0,0 +1,71 @@
+module.exports = class HtmlRenderer {
+
+	renderMysqlDetails(data) {
+		var s = '';
+		if(data) {
+			s += '<div class="col-xs-12 col-sm-6 col-lg-4">';
+			s += '<div class="panel panel-default">';
+			s += '<div class="panel-heading"><h4 class="panel-title">' + data.name + '</h4></div>';
+			s += '<div class="panel-body">';
+
+			s += '<div class="input-group"><span class="input-group-addon">Host</span><input type="text" name="mysql-host" value="' + data.host + '" /></div>';
+			s += '<div class="input-group"><span class="input-group-addon">Database Name</span><input type="text" name="mysql-database" value="' + data.database + '" /></div>';
+			s += '<div class="input-group"><span class="input-group-addon">Userame</span><input type="text" name="mysql-username" value="' + data.username + '" /></div>';
+			s += '<div class="input-group"><span class="input-group-addon">Password</span><input type="password" name="mysql-password" value="' + data.password + '" /></div>';
+			s += '<button class="btn btn-default pull-right connect-to-db-button" type="submit">Connect</button>';
+			s += '</div></div></div>';
+		}
+		return s;
+	}
+
+	renderServers(data) {
+	 	var s = '';
+	 	if(data.servers) {
+	 		var servers = data.servers;
+	 		s += '<ul class="list-group server-list-group">';
+	 		for(var i in servers) {
+	 			var server = servers[i];
+	 			if(server) {
+	 				s += '<li class="list-group-item"><span class="server-name">' + server.name + '</span>';
+	 				s += '<div class="shortcut-buttons">' + html.renderShortcuts(server.connections) + '</div>';
+	 				s += '<span class="badge">' + server.connections.length + '</span>';
+	 				s += '<div class="server-details" style="display: none;">';
+	 				s += this.renderServerDetails(server);
+	 				s += '</div><button class="btn btn-default server-update-button pull-right" style="display: none;">Update Server Data</button><div class="clear-fix"></div></li>';
+	 			}
+	 		}
+	 		s += '</ul>';
+	 	}
+	 	return s;
+	}
+
+	renderServerDetails(data) {
+		var s = '';
+		if(data) {
+			s += '<div class="input-group"><span class="input-group-addon">Host</span><input type="text" name="ssh-host" value="' + data.host + '" /></div>';
+			s += '<div class="input-group"><span class="input-group-addon">Port</span><input type="text" name="ssh-port" value="' + data.port + '" /></div>';
+			s += '<div class="input-group"><span class="input-group-addon">Username</span><input type="text" name="ssh-username" value="' + data.username + '" /></div>';
+			s += '<div class="input-group"><span class="input-group-addon">Password</span><input type="password" name="ssh-password" value="' + data.password + '" /></div>';
+			if(data.connections) {
+				s += '<div class="panel panel-default">';
+				s += '<div class="panel-heading"><h3 class="panel-title">Database Connections</h3></div>';
+				s += '<div class="panel-body">';
+				for(var i in data.connections) {
+					s+= this.renderMysqlDetails(data.connections[i]);
+				}
+				s += '</div></div>';
+			}
+		}
+		return s;
+	}
+	
+	renderShortcuts(data) {
+		var s = '';
+		for(var i in data) {
+			if(data[i] && data[i].name) {
+				s += '<button class="btn btn-default shortcut-button connect-to-db-shortcut-button" data-db="' + data[i].database + '">' + data[i].name + '</button>';
+			}
+		}
+		return s;
+	}
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+const HtmlRenderer = require('./custom_modules/HtmlRenderer.js');
+
+let html = new HtmlRenderer();
+
 const tunnel = require('tunnel-ssh');
 const mysql = require('mysql');
 
@@ -46,74 +50,6 @@ module.exports = {
 	]
 }
  **/
-var renderShortcuts = function(data) {
-	var s = '';
-	for(var i in data) {
-		if(data[i] && data[i].name) {
-			s += '<button class="btn btn-default shortcut-button connect-to-db-shortcut-button" data-db="' + data[i].database + '">' + data[i].name + '</button>';
-		}
-	}
-	return s;
-}
-
-var renderMysqlDetails = function(data) {
-	var s = '';
-	if(data) {
-		s += '<div class="col-xs-6">';
-		s += '<div class="panel panel-default">';
-		s += '<div class="panel-heading"><h4 class="panel-title">' + data.name + '</h4></div>';
-		s += '<div class="panel-body">';
-
-		s += '<div class="input-group"><span class="input-group-addon">Host</span><input type="text" name="mysql-host" value="' + data.host + '" /></div>';
-		s += '<div class="input-group"><span class="input-group-addon">Database Name</span><input type="text" name="mysql-database" value="' + data.database + '" /></div>';
-		s += '<div class="input-group"><span class="input-group-addon">Userame</span><input type="text" name="mysql-username" value="' + data.username + '" /></div>';
-		s += '<div class="input-group"><span class="input-group-addon">Password</span><input type="password" name="mysql-password" value="' + data.password + '" /></div>';
-		s += '<button class="btn btn-default pull-right connect-to-db-button" type="submit">Connect</button>';
-		s += '</div></div></div>';
-	}
-	return s;
-}
-
-var renderServerDetails = function(data) {
-	var s = '';
-	if(data) {
-		s += '<div class="input-group"><span class="input-group-addon">Host</span><input type="text" name="ssh-host" value="' + data.host + '" /></div>';
-		s += '<div class="input-group"><span class="input-group-addon">Port</span><input type="text" name="ssh-port" value="' + data.port + '" /></div>';
-		s += '<div class="input-group"><span class="input-group-addon">Username</span><input type="text" name="ssh-username" value="' + data.username + '" /></div>';
-		s += '<div class="input-group"><span class="input-group-addon">Password</span><input type="password" name="ssh-password" value="' + data.password + '" /></div>';
-		if(data.connections) {
-			s += '<div class="panel panel-default">';
-			s += '<div class="panel-heading"><h3 class="panel-title">Database Connections</h3></div>';
-			s += '<div class="panel-body">';
-			for(var i in data.connections) {
-				s+= renderMysqlDetails(data.connections[i]);
-			}
-			s += '</div></div>';
-		}
-	}
-	return s;
-}
-
-var renderServers = function(data) {
- 	var s = '';
- 	if(data.servers) {
- 		var servers = data.servers;
- 		s += '<ul class="list-group server-list-group">';
- 		for(var i in servers) {
- 			var server = servers[i];
- 			if(server) {
- 				s += '<li class="list-group-item"><span class="server-name">' + server.name + '</span>';
- 				s += '<div class="shortcut-buttons">' + renderShortcuts(server.connections) + '</div>';
- 				s += '<span class="badge">' + server.connections.length + '</span>';
- 				s += '<div class="server-details" style="display: none;">';
- 				s += renderServerDetails(server);
- 				s += '</div><button class="btn btn-default server-update-button pull-right" style="display: none;">Update Server Data</button><div class="clear-fix"></div></li>';
- 			}
- 		}
- 		s += '</ul>';
- 	}
- 	return s;
-}
 
 $(document).on("click", ".server-list-group .server-name", function (evt) {
 	var $parent = $(this).closest(".list-group-item");
@@ -159,7 +95,7 @@ $(document).ready(function() {
  	$.ajax('./working_files/config.json').done(function(data) {
  		if(data) {
  			config = JSON.parse(data);
- 			$(".server-list").html(renderServers(config));
+ 			$(".server-list").html(html.renderServers(config));
  		}
  	});
 
